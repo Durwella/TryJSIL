25550535c37b1606ed0e079fd9fb1e3ef4fb3c72
Jacob Foshee <jacobf@gmail.com>
2015-03-26 17:09:32 -0500

    Doing some math

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,6 +7,11 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Javascript!");
+            var random = new Random();
+            int n = random.Next();
+            Console.WriteLine("{0} is a randomly generated {1}", n, n.GetType());
+            int log = (int)Math.Floor( Math.Log10( Math.Abs(n) ) );
+            Console.WriteLine("Floor(Log10(|{0}|)) = {1}.", n, log);
         }
     }
 }
